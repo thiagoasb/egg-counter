9c870250dd453b5cf3b5e32887d9998119a50631
thiagoasb <thiagoasbezerra@gmail.com>
2022-08-31 22:23:22 -0300

    Add toolbar menu and change theme color

--- app/src/main/java/com/thiagoaraujo/eggcounter/MainActivity.kt
+++ app/src/main/java/com/thiagoaraujo/eggcounter/MainActivity.kt
@@ -4,7 +4,10 @@ import android.content.Context
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.thiagoaraujo.eggcounter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -39,11 +42,8 @@ class MainActivity : AppCompatActivity() {
         }
 
         binding.ivEgg.setOnLongClickListener {
-            Toast.makeText(
-                this,
-                getString(R.string.count_eggs_message, secretNumber.toString()),
-                Toast.LENGTH_LONG
-            ).show()
+            resetState()
+            Toast.makeText(this, "Counter's egg restarted", Toast.LENGTH_SHORT).show()
             true
         }
 
@@ -79,5 +79,38 @@ class MainActivity : AppCompatActivity() {
 
     private fun getSecretNumber(): Int {
         return (1..30).random()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.app_bar_manu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.show_counter -> createEggCounterDialog()
+            R.id.show_info -> createInfoDialog()
+        }
+        return true
+    }
+
+    private fun createEggCounterDialog() {
+        val seeRandomNumber = AlertDialog.Builder(this)
+            .setTitle("Hint")
+            .setMessage(getString(R.string.count_eggs_message, secretNumber.toString()))
+            .setPositiveButton("OK") { dialogInterface, _ ->
+                dialogInterface.dismiss()
+            }.create()
+        seeRandomNumber.show()
+    }
+
+    private fun createInfoDialog() {
+        val seeRandomNumber = AlertDialog.Builder(this)
+            .setTitle("Info")
+            .setMessage("Click until the secret number is reached, hold to reset the counter and if wanted see the secret number on the eye icon")
+            .setPositiveButton("OK") { dialogInterface, _ ->
+                dialogInterface.dismiss()
+            }.create()
+        seeRandomNumber.show()
     }
 }
